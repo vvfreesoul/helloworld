a87ac921fefec20e38fb2fddb379dff32515441d
zhangguowei <285009003@qq.com>
2020-07-14 22:45:31 +0800

    update

--- 32minist.py
+++ 32minist.py
@@ -24,7 +24,7 @@ def main():
     args = parser.parse_args()
     args.world_size = args.gpus * args.nodes
     os.environ['MASTER_ADDR'] = '10.151.40.224'
-    os.environ['MASTER_PORT'] = '19737'
+    os.environ['MASTER_PORT'] = '8888'
     mp.spawn(train, nprocs=args.gpus, args=(args,))
 
 

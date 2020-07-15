da7b53a5e39fb6d78f7c3a0038a098d89fabf06e
zhangguowei <285009003@qq.com>
2020-07-15 12:57:20 +0800

    update

--- PrintIP.py
+++ PrintIP.py
@@ -8,6 +8,7 @@ def get_ip_address(ifname):
     return socket.inet_ntoa(fcntl.ioctl(
         s.fileno(),
         0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
+        struct.pack('256s', ifname[:15].encode("utf-8"))
     )[20:24])
-get_ip_address('lo')
+print(get_ip_address('lo'))
+print(get_ip_address('eth0'))
